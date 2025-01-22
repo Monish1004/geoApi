c18b70ddcbdbd8c70504dbc140e35cbecdea5a49
Monish1004 <22h51a05q5@cmrcet.ac.in>
2025-01-22 11:18:15 +0530

    Heje

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     return res.send('Hello World');
