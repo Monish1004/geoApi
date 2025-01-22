584c8b1d0efe9817e3c46af08c90b3899c921123
Monish1004 <22h51a05q5@cmrcet.ac.in>
2025-01-22 10:54:12 +0530

    Hehe

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+const port = process.env.port || 3000;
+
+app.get('/', (req, res) => {
+    return res.send('Hello World');
+})
+
+app.post('/getReq' , (req , res) => {
+    try {
+        const data = req.body;
+        console.log(data);
+        return res.status(200).send(data);
+    } catch (error) {
+        return res.status(500).send("Error");
+    }
+})
+
+app.listen(port , () => {
+    console.log('Server started at ' , port);
+})
